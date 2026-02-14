74fdf95f12aad7162847283454513dfb8c7f0270
Kevin Wu <kevinwu098@gmail.com>
2026-02-13 21:40:20 -0800

    feat: init website

--- apps/website/src/app/layout.tsx
+++ apps/website/src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import React from "react";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -25,7 +24,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        {children}
+      </body>
     </html>
   );
 }
